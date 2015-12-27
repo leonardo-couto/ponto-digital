485b5a391ab5f4186a3f39e18d4b0819b76dd0b3
Leonardo Couto <leonardo.couto@gmail.com>
2015-12-27 14:15:15 -0200

    Updated Allocation screen

--- app/src/main/java/com/github/leonardocouto/pontodigital/MainActivityFragment.java
+++ app/src/main/java/com/github/leonardocouto/pontodigital/MainActivityFragment.java
@@ -1,9 +1,9 @@
 package com.github.leonardocouto.pontodigital;
 
 import android.graphics.Color;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,14 +28,28 @@ public class MainActivityFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
+        /*
+        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "MaterialIcons-Regular.ttf");
+        TextView text = (TextView) view.findViewById(R.id.add_new_activity_sign);
+        text.setTypeface(font);
+        */
+
+        LinearLayout addNewActivity = (LinearLayout) view.findViewById(R.id.add_new_activity);
+        addNewActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: open activity chooser fragment
+            }
+        });
+
         this.makeChart(view);
         return view;
     }
 
     private void makeChart(View view) {
 
-        double[] distribution = {85.0d, 15.0d};
-        int[] colors = {Color.argb(255, 165, 198, 99), Color.argb(255,15,88,55)};
+        double[] distribution = {45.0d, 40.0d, 15.0d};
+        int[] colors = {Color.argb(255, 142, 170, 85), Color.argb(255, 165, 198, 99), Color.argb(255,121,144,72)};
 
         CategorySeries distributionSeries = new CategorySeries("Allocation");
 
@@ -60,16 +74,16 @@ public class MainActivityFragment extends Fragment {
 
     @NonNull
     private DefaultRenderer allocationChartRenderer() {
-        DefaultRenderer defaultRenderer = new DefaultRenderer();
-        defaultRenderer.setApplyBackgroundColor(false);
-        defaultRenderer.setShowLabels(false);
-        defaultRenderer.setShowLegend(false);
-        defaultRenderer.setZoomButtonsVisible(false);
-        defaultRenderer.setClickEnabled(false);
-        defaultRenderer.setZoomEnabled(false);
-        defaultRenderer.setInScroll(false);
-        defaultRenderer.setPanEnabled(false);
-        defaultRenderer.setScale(1.25f);
-        return defaultRenderer;
+        DefaultRenderer renderer = new DefaultRenderer();
+        renderer.setApplyBackgroundColor(false);
+        renderer.setShowLabels(false);
+        renderer.setShowLegend(false);
+        renderer.setZoomButtonsVisible(false);
+        renderer.setClickEnabled(false);
+        renderer.setZoomEnabled(false);
+        renderer.setInScroll(false);
+        renderer.setPanEnabled(false);
+        renderer.setScale(1.25f);
+        return renderer;
     }
 }
